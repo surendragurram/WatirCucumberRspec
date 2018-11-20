67602c608d69a841775840e9695d966b0a579087
surendragurram <sgurram36@gmail.com>
2018-11-20 09:34:16 +0100

    again test

--- features/step_definitions/create_ownsites_steps.rb
+++
@@ -1,9 +0,0 @@
-Given("I create new location with Own site") do
-  @browser.windows.last.use
-  visit(CreateOwnsitePage)
-  on(CreateOwnsitePage).own_site('Standort qm41 manuell', '00177', 'x', 'Cairns')
-end
-
-Then("I should see {string}") do |string|
-    expect(@browser.text).to include "Risk Object created."
-end
